3db2e34413e04a0b898c0f55fe4e2bff25e9e6f4
tommyyangdev <tommyyang.dev@gmail.com>
2025-09-15 19:03:27 +1000

    Update Assignment-2.cpp

--- Assignment-2/Assignment-2.cpp
+++ Assignment-2/Assignment-2.cpp
@@ -35,14 +35,77 @@ using namespace std;
 /// add each path as a string into std::set<std::string> paths
 /// Print the path in the format "START->1->2->4->5->END", where -> indicate an ICFGEdge connects two ICFGNode IDs
 
-void ICFGTraversal::collectICFGPath(std::vector<unsigned> &path){
-    
+void ICFGTraversal::collectICFGPath(std::vector<unsigned> &path) {
+    std::string newPath = "START->";
+    for (auto nodeID : path) { 
+        newPath += std::to_string(nodeID);
+        newPath += "->";
+    }
+    newPath += "END";
+    std::cout << newPath << std::endl;
+    paths.insert(newPath);
 }
 
 
 /// TODO: Implement your context-sensitive ICFG traversal here to traverse each program path (once for any loop) from src to dst
-void ICFGTraversal::reachability(const ICFGNode *src, const ICFGNode *dst)
-{
-    
+void ICFGTraversal::reachability(const ICFGNode *src, const ICFGNode *dst) {
+    auto newPair = std::make_pair(src, callstack);
+    if (visited.find(newPair) != visited.end()) {
+        return;
+    }
+    visited.insert(newPair);
+    path.push_back(src -> getId());
 
+    if (src == dst) {
+        collectICFGPath(path);
+    }
+    
+    for (const ICFGEdge *edge : src->getOutEdges()) {
+        if (edge -> isIntraCFGEdge()) {
+            reachability(edge -> getDstNode(), dst);
+        }
+        else if (edge -> isCallCFGEdge()) {
+            callstack.push_back(edge -> getSrcNode());
+            reachability(edge -> getDstNode(), dst);
+            callstack.pop_back();
+        }
+        else if (edge -> isRetCFGEdge()) {
+            const RetCFGEdge *RetEdge = SVFUtil::dyn_cast<RetCFGEdge>(edge);
+            if (callstack.empty() == false && callstack.back() == RetEdge -> getCallSite()) {
+                callstack.pop_back();
+                reachability(RetEdge -> getDstNode(), dst);
+                callstack.push_back(RetEdge -> getCallSite());
+            }
+            else if (callstack.empty() == true) {
+                reachability(RetEdge -> getDstNode(), dst);
+            }
+        }
+    }
+    visited.erase(newPair);
+    path.pop_back();
 }
+
+/*pair = ⟨curNode, callstack⟩;
+    if pair ∈ visited then
+        return;
+    visited.insert(pair);
+    path.push back(curNode);
+
+    if src == snk then
+        collectICFGPath(path);
+    foreach edge ∈ curNode.getOutEdges() do
+        if edge.isIntraCFGEdge() then
+            reachability(edge.dst, snk);
+        else if edge.isCallCFGEdge() then
+            callstack.push back(edge.src);
+            reachability(edge.dst, snk);
+            callstack.pop back();
+        else if edge.isRetCFGEdge() then
+            if callstack ̸ = ∅ && callstack.back() == edge.getCallSite() then
+                callstack.pop back();
+                reachability(edge.dst, snk);
+                callstack.push back(edge.getCallSite());
+            else if callstack == ∅ then
+                reachability(edge.dst, snk);
+    visited.erase(pair);
+    path.pop back();*/
